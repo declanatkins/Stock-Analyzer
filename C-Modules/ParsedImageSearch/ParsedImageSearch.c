b4ce98784636d89e25ea80d893ce1da578815b85
Atkins <datkin10@ms.ds.uhc.com>
2017-08-24 15:55:28 +0100

    Added get actual values func to parsed image search

--- C-Modules/ParsedImageSearch/ParsedImageSearch.c
+++ C-Modules/ParsedImageSearch/ParsedImageSearch.c
@@ -18,8 +18,10 @@ on the x axis and price on the y axis.
 #include <stdlib.h>
 #include <stdbool.h>
 
-int **get_xy_values(int **image, int x_size, int y_size);
+int *get_xy_values(int **image, int x_size, int y_size);
 bool all_zero(int *pos);
+double *get_actual_values(int *y_vals, int y_size, double val1, int val1_y, double val2, int val2_y);
+
 
 
 
@@ -35,9 +37,9 @@ bool all_zero(int *pos){
 }
 
 
-int **get_xy_values(int **image, int x_size, int y_size){
+int *get_xy_values(int **image, int x_size, int y_size){
 	
-	int** values;
+	int* values;
 	int pos = 0;
 	int i,j;
 	for(i=0;i<y_size;i++){
@@ -46,20 +48,20 @@ int **get_xy_values(int **image, int x_size, int y_size){
 			if(!all_zero(image[i][j]){
 				
 				if(i>0 && all_zero(image[i-1][j])){
-					values = realloc(values,2*sizeof(int))
-					values[pos++] = {j,i};
+					values = realloc(values,sizeof(int))
+					values[pos++] = i;
 				}
 				else if(j>0 && all_zero(image[i][j-1])){
-					values = realloc(values,2*sizeof(int))
-					values[pos++] = {j,i};
+					values = realloc(values,sizeof(int))
+					values[pos++] = i;
 				}
 				else if(j<x_size-1 && all_zero(image[i][j+1])){
-					values = realloc(values,2*sizeof(int))
-					values[pos++] = {j,i};
+					values = realloc(values,sizeof(int))
+					values[pos++] = i;
 				}
 				else if(i<y_size-1 && all_zero(image[i+1][j])){
-					values = realloc(values,2*sizeof(int))
-					values[pos++] = {j,i};
+					values = realloc(values,sizeof(int))
+					values[pos++] = i;
 				}
 				
 			}
@@ -70,3 +72,22 @@ int **get_xy_values(int **image, int x_size, int y_size){
 	return values;
 }
 
+double *get_actual_values(int *y_vals, int y_size, double val1, int val1_y, double val2, int val2_y){
+	
+	int i;
+	double val_diff = val2 - val1;
+	int y_diff = val2_y - val1_y;
+	double val_per_y = val_diff/y_diff;
+
+	double *ret_values = (double*) malloc(sizeof(double));
+	int pos=0;
+	for(i=0;i<y_size;i++){
+
+		y_diff = y_vals[i] - val1_y;
+		ret_values = realloc(ret_values, sizeof(double));
+
+		ret_values[pos++] = y_diff * val_per_y;
+	}
+
+	return ret_values;
+}
