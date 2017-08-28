1771727636f27d6783060cb5e48408e3a1f7a585
Declan Atkins <declan.atkins@ucdconnect.ie>
2017-08-28 17:30:06 +0100

    Added Company Class

--- Python/ModelHandler.py
+++ Python/ModelHandler.py
@@ -2,7 +2,39 @@ import sys
 
 sys.path.append("../C-Modules/ModelOperations")
 import ModelOperations
+import os
 
-val = ModelOperations.get_predicted_values("Amazon", "Neutral")
-val = ModelOperations.doubleArray_frompointer(val)
-print(val[0])
+class KeywordException(Exception):
+    pass
+
+class Company:
+
+    def __init__(name):
+        self.name = name
+        self.currentKeywordSet = ""
+    
+    def updateKeywordSet(self,currSet):
+        if currSet not in os.listdir('../Data/'+ self.name + '/'):
+            os.mkdir('../Data/' + self.name + '/' + currSet)
+        self.currentKeywordSet = currSet
+
+    def getPredictedVals(self):
+        self.testKeywordError()
+        self.predictedVals = ModelOperations.get_predicted_values(self.name, self.currentKeywordSet)
+        return self.predictedVals
+    
+    def updateVals(self,listVals):
+        self.testKeywordError()
+        arr = ModelOperations.doubleArray(len(listVals))
+        for i,val in enumerate(listVals):
+            arr[i] = val
+        ModelOperations.add_values(arr, self.currentKeywordSet)
+
+    def getNextPredictedValue(self):
+        self.testKeywordError()
+        return ModelOperations.make_single_prediction_EXTERNAL(self.name,self.currentKeywordSet)
+
+    def testKeywordError(self):
+        if self.currentKeywordSet not in os.listdir'../Data/'+ self.name + '/'):
+            raise KeywordException('Not a valid keyword set')
+
