e551269d05f441a3034ba0bbb62529559bf09fb7
Declan Atkins <declan.atkins@ucdconnect.ie>
2017-08-29 18:35:25 +0100

    Adding graph handler module

--- Python/GraphHandler.py
+++ Python/GraphHandler.py
@@ -6,52 +6,63 @@ This module is used to parse the data from graphs
 that are taken in.
 
 """
-import sys
-sys.path.append("../C-Modules/ParsedImageSearch")
-import ParsedImageSearch as imgSearch
+import re
 import cv2
 import numpy as np
-import copy
-import matplotlib.pyplot as pyp
 from urllib import request
 from newspaper import Article
 
-class GraphHandler:
-
-    def __init__(self, companyAbbrv):
-        self.abbrv = companyAbbrv
-        self.valsList = []
-        self.graphFilename = '../Data/graphs/' + self.abbrv + '.gif'
 
 
-    def pullGraph(self):
-        graph = open(self.graphFilename)
+class GraphException(Exception):
+    pass 
+
+class GraphDataExtractor:
+
+    def __init__(self, companyName,companyAbbr):
+        self.abbrv = companyAbbr
+        self.name = companyName
+        self.imgPath = '../Data/Graphs/' + self.name + '.jpg'
+
+    def pullGraphFromSite(self):
         url = 'http://www.nasdaq.com/symbol/' + self.abbrv + '/stock-chart?intraday=on&timeframe=intra'
-        a = Article(url)
-        a.download()
-        imgs = a.fetch_images()
-        for img in imgs:
-            print(img)
-    
-    def getXY(self):
-        img = cv2.imread(self.graphFilename)
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        lower = np.array([0,120,150])
-        upper = np.array([255,255,255])
-
-        mask = cv2.inRange(hsv,lower,upper)
-        res = cv2.bitwise_and(img,img,mask=mask)
-
-        resOuterArray = imgSearch.intArrayArray(len(res))
-        for pixel in res:
-            thisResPixel = imgSearch.intArray(len(pixel))
-            for i in range(len(pixel)):
-                thisResPixel[i] = pixel[i]
         
-        self.xyArray = imgSearch.get_xy_values(resOuterArray)
+        try:
+            graphWebPage = Article(url)
+            graphWebPage.download()
+            htmlStr = graphWebPage.html
+        except newspaper.ArticleException:
+            raise GraphException('Couldnt download webpage') 
 
+        while True:
+            if not re.search('<img', htmlStr):
+                raise GraphException('Chart not on page')
+            pre,sep,block = htmlStr.partition('<img')
+            img,sep,htmlStr = block.partition('>')
+
+            if 'stock chart' in img:
+                imgURL = re.search('\"(.+?)\"', img).group(1)
+                with open(self.imgPath, 'wb') as imgFile:
+                    imgFile.write(request.urlopen(imgURL).read())
+                break
+            else:
+                continue
+    
+    def cropImage(self):
+        import os
+        print(os.getcwd())
+        img = cv2.imread('C:/Users/Declan/Desktop/Stock-AnalyzerData/Graphs/amazon.jpg', 1)
+        #img = cv2.imread(self.imgPath)
+        cv2.imshow('precrop',img)
+        #cv2.waitkey(0)
+        #cropImg = img[70:30, 550:100]
+        #cv2.imshow('cropped',cropImg)
+        #cv2.waitkey(0)
 
 if __name__ == '__main__':
+    import os
+    l = os.listdir('../data/graphs')
+    print("{}".format(l))
 
-    gh = GraphHandler('amzn')
-    gh.pullGraph()
+
+
