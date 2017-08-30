0523bac8cac2152df51cd89c49cffab39af39d50
Declan Atkins <declan.atkins@ucdconnect.ie>
2017-08-30 15:51:17 +0100

    Added centre bar search

--- Python/GraphHandler.py
+++ Python/GraphHandler.py
@@ -43,20 +43,41 @@ class GraphDataExtractor:
                 imgURL = re.search('\"(.+?)\"', img).group(1)
                 with open(self.imgPath + '.gif','wb') as imgFile:
                     imgFile.write(request.urlopen(imgURL).read())
+                #convert image from gif to png for use with cv2
+                img = Image.open(self.imgPath + '.gif')
+                new_im = Image.new("RGBA", img.size)
+                new_im.paste(img)
+                new_im.save(self.imgPath +'.png')
+                self.imgPath += '.png'
                 break
             else:
                 continue
     
     def cropImage(self):
-        img = Image.open(self.imgPath + '.gif')
-        new_im = Image.new("RGBA", img.size)
-        new_im.paste(img)
-        new_im.save(self.imgPath +'.png')
-        graph = cv2.imread(self.imgPath +'.png')
-        print(graph)
+        if not re.search('.png', self.imgPath):
+            raise GraphException('Image has not been converted')
+        
+        fullImage = cv2.imread(self.imgPath)
+        upperCrop = fullImage[25:128,108:488]
+        lowerCrop = fullImage[132:,108:488]
+        cv2.imshow('crop',lowerCrop)
+        cv2.waitKey(0)
+    
+    def getCentreBarPos(self):
+        img = cv2.imread(self.imgPath)
+        img = img[30:200,108:109]
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        lower = np.array([0,0,0])
+        upper = np.array([200,200,200])
+        mask = cv2.inRange(hsv,lower,upper)
+        res = cv2.bitwise_and(img,img,mask=mask)
+        for i,pixel in enumerate(res):
+            if pixel[0,0] != 0:
+                return i+1
 
 
 if __name__ == '__main__':
     gde = GraphDataExtractor('Amazon', 'amzn')
     #gde.pullGraphFromSite()
-    gde.cropImage()
+    gde.imgPath += '.png'
+    gde.getCentreBarPos()
